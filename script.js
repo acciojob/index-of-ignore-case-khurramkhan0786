be9587750dc97e68ad63181ca06e91cf985a775a
khurram khan <153985864+khurramkhan0786@users.noreply.github.com>
2025-02-28 18:16:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	let str = s1.toLowerCase();
+	let substr = s2.toLowerCase();
+	for(int i = 0;i<str.length - substr.length;i++){
+        let found = true;
+	for(int j=0;j<substr.length;j++){
+		if(str[i+j] !== substr[j]){
+			found = false;
+			break;
+		}
+	}
+		if(found) return i;
+    }
+	return -1;
 }
 
 // Please do not change the code below
